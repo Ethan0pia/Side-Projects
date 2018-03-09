957798687bb4c90cb2ef6a533387b68ab0301f26
Ethan Dunham <ethan.dunham@gmail.com>
2018-03-09 12:01:35 -0500

    Add files via upload

---
+++ Udemy - Angular 5/Assignment3/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  buttonClicked = false;
+  clicks = 0;
+  arrClicks = [];
+
+  buttonWasClicked() {
+    this.buttonClicked = true;
+    this.clicks++;
+    this.arrClicks.push(this.clicks);
+  }
+
+}
